cd166ee22e9444ea719fb7710b353f1944251d6b
Mel-rox <102379203+Mel-rox@users.noreply.github.com>
2022-04-05 11:32:29 +0800

    Update settings.py

--- plantita_site/settings.py
+++ plantita_site/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)ijkr(8zowe2-m=4l#!7&o#md^(42t+(xsl*7f1k@xjw4t%z3!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
