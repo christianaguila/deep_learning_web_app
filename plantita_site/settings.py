39232a1bcce06ba22d8df911a02687e2c1667989
Mel-rox <102379203+Mel-rox@users.noreply.github.com>
2022-04-09 10:59:37 +0800

    Update settings.py

--- plantita_site/settings.py
+++ plantita_site/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 ]
 
 CSRF_COOKIE_SECURE = True
+CSRF_TRUSTED_ORIGINS=['https://plantita.azurewebsites.net'] 
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
